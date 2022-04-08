81159ea9523ab4f723f63b214ede65474e2a120d
Matías Guarnera <76770019+matiasg11@users.noreply.github.com>
2022-04-08 10:03:29 -0300

    Create removeEveryOther.js

---
+++ 8kyu/removeEveryOther.js
@@ -0,0 +1,18 @@
+/*Description:
+Take an array and remove every second element from the array. Always keep the first element and start removing with the next element.
+
+Example:
+
+["Keep", "Remove", "Keep", "Remove", "Keep", ...] --> ["Keep", "Keep", "Keep", ...]
+
+None of the arrays will be empty, so you don't have to worry about that! */
+
+function removeEveryOther(arr){
+  let array = []
+  for (let i=0; i<arr.length; i++){
+    if (i%2 ==0){
+      array.push(arr[i])
+    }
+  }
+  return array
+}
