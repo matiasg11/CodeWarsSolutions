7692315c3aee902bab184a3d7f1d43ced7717529
Matías Guarnera <76770019+matiasg11@users.noreply.github.com>
2022-04-11 20:35:07 -0300

    Create howGoodAreYouReally.js

---
+++ 8kyu/howGoodAreYouReally.js
@@ -0,0 +1,25 @@
+/*There was a test in your class and you passed it. Congratulations!
+But you're an ambitious person. You want to know if you're better than the average student in your class.
+
+You receive an array with your peers' test scores. Now calculate the average and compare your score!
+
+Return True if you're better, else False!
+
+Note:
+Your points are not included in the array of your class's points. For calculating the average point you may add your point to the given array!*/
+
+function betterThanAverage(classPoints, yourPoints) {
+  let sumPoints = 0;
+  for (let i=0; i<classPoints.length; i++){
+    sumPoints += classPoints[i]
+  }
+  sumPoints+=yourPoints
+  
+  let average = sumPoints/(classPoints.length+1)
+  if (average<yourPoints){
+    return true
+  } else {
+    return false
+  }
+  
+}
