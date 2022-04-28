eb4cb8a1909fd69cdf236e5a1da74277f4e2b3e9
Matías Guarnera <76770019+matiasg11@users.noreply.github.com>
2022-04-28 10:31:34 -0300

    Create zerosToEnd.js

---
+++ 5kyu/zerosToEnd.js
@@ -0,0 +1,14 @@
+/*Write an algorithm that takes an array and moves all of the zeros to the end, preserving the order of the other elements.
+
+moveZeros([false,1,0,1,2,0,1,3,"a"]) // returns[false,1,1,2,1,3,"a",0,0]*/
+
+function moveZeros(arr) {
+  let zeros = 0
+    for(let i = arr.length; i>=0 ; i--) {
+    if(arr[i]===0) {
+      arr.splice(i, 1);
+      arr.push(0);
+    }
+  } return arr;
+}
+//El truco está en correr el array para atrás.
