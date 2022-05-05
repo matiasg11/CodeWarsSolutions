7e55b494cb16366193dc39feb9759f87d7e56052
Matías Guarnera <76770019+matiasg11@users.noreply.github.com>
2022-05-05 20:20:56 -0300

    Create iceBrick.js

---
+++ 8kyu/iceBrick.js
@@ -0,0 +1,12 @@
+/*Task:
+A magician in the subway showed you a trick, he put an ice brick in a bottle to impress you. The brick's length and width are equal, forming a square; its height may be different. Just for fun and also to impress the magician and people around, you decided to calculate the brick's volume. Write a function iceBrickVolume that will accept these parameters:
+
+radius - bottle's radius (always > 0);
+bottleLength - total bottle length (always > 0);
+rimLength - length from bottle top to brick (always < bottleLength);
+And return volume of ice brick that magician managed to put into a bottle.*/
+
+function iceBrickVolume(radius, bottleLength, rimLength) {
+return (radius**2)*(bottleLength-rimLength)*2}
+
+//Stupid thing: It gives you a radius but considers a square
