a8f722717506f85d51c9b1b167ff0d90382cd3bf
Matías Guarnera <76770019+matiasg11@users.noreply.github.com>
2022-05-16 20:19:35 -0300

    Create digitalRoot.js

---
+++ 6kyu/digitalRoot.js
@@ -0,0 +1,22 @@
+/*Digital root is the recursive sum of all the digits in a number.
+
+Given n, take the sum of the digits of n. If that value has more than one digit, continue reducing in this way until a single-digit number is produced. The input will be a non-negative integer.
+
+Examples
+    16  -->  1 + 6 = 7
+   942  -->  9 + 4 + 2 = 15  -->  1 + 5 = 6
+132189  -->  1 + 3 + 2 + 1 + 8 + 9 = 24  -->  2 + 4 = 6
+493193  -->  4 + 9 + 3 + 1 + 9 + 3 = 29  -->  2 + 9 = 11  -->  1 + 1 = 2*/
+
+function digital_root(n) {
+  let result = 0
+  let num = n.toString()
+  for(let i = 0; i < num.length; i++) {
+    result += parseInt(num[i]);
+  }
+  return result < 10 ? result : digital_root(result);  //Using ternary now that I totally got it
+}
+
+function digital_root(n) {
+  return (n - 1) % 9 + 1;
+}
