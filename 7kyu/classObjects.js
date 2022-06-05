a34b50497acdaca325646073676581906051a505
Matías Guarnera <76770019+matiasg11@users.noreply.github.com>
2022-06-05 10:45:20 -0300

    Create classObjects.js

---
+++ 7kyu/classObjects.js
@@ -0,0 +1,12 @@
+/*For this exercise you should create a JavaScript class like object called "Animal" that takes in "name" and "type" arguments. It should have a toString method that returns a human readable string indicating the argument information passed in. It should also allow the name property to be set.*/
+
+// create your Animal class like object here
+class Animal  {
+    constructor(name, type){
+        this.name = name;
+        this.type = type;
+    }
+    toString() {
+        return `${this.name} is a ${this.type}`;
+    }
+}
