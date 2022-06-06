1fe39fd2d651d360d70c3225126d183839c3ee11
Matías Guarnera <76770019+matiasg11@users.noreply.github.com>
2022-06-06 12:55:50 -0300

    Create toBin.js

---
+++ 8kyu/toBin.js
@@ -0,0 +1,5 @@
+//Given a non-negative integer n, write a function to_binary/ToBinary which returns that number in a binary format.
+function toBinary(n){
+  let N = parseInt(n.toString(2))
+  return N;
+}
