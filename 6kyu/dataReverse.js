fb930bd1edcc21e94125bf0ed68f30704ec12a36
Matías Guarnera <76770019+matiasg11@users.noreply.github.com>
2022-07-07 09:54:53 -0300

    Create dataReverse.js

---
+++ 6kyu/dataReverse.js
@@ -0,0 +1,10 @@
+function dataReverse(data) {
+   let bytes = []
+   while (data.length>0){
+     bytes.push(data.slice(0,8))
+     data.splice(0,8)
+   }
+  let result = bytes.reverse()
+  return result
+}
+//falta el ultimo giro
