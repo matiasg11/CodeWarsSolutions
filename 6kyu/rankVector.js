bf77b0a87e87436d0c5868dfcd21f9c5f8188fe8
Matías Guarnera <76770019+matiasg11@users.noreply.github.com>
2022-07-10 18:34:12 -0300

    Update rankVector.js

--- 6kyu/rankVector.js
+++ 6kyu/rankVector.js
@@ -1,1 +1,10 @@
-//TbC
+function ranks(array) {
+  let arrSorted = []
+  for(let i=0; i<array.length; i++){
+    arrSorted[i] = array[i]
+  }
+  arrSorted = arrSorted.sort((a,b)=>b-a)
+  
+  let result = array.map((m)=> arrSorted.indexOf(m)+1)
+  return result
+}
