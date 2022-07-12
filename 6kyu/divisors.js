d658a22265c68425b731e016aeb6349052ac57b6
Matías Guarnera <76770019+matiasg11@users.noreply.github.com>
2022-07-12 09:00:16 -0300

    Create divisors.js

---
+++ 6kyu/divisors.js
@@ -0,0 +1,7 @@
+function divisors(integer) {
+  let result = [];
+  for(let i=2; i<integer; i++){
+    if(integer % i == 0) result.push(i);
+  }
+  return result.length == 0 ? `${integer} is prime` : result;
+};
