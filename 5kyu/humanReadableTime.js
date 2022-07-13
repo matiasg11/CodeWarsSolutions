23b5be7bbc4ba9b90ffe4619de246c0d3ee544a0
Matías Guarnera <76770019+matiasg11@users.noreply.github.com>
2022-07-13 09:59:45 -0300

    Create humanReadableTime.js

---
+++ 5kyu/humanReadableTime.js
@@ -0,0 +1,19 @@
+function humanReadable (seconds) {
+  let hours = Math.floor(seconds/3600)
+  let minutes = Math.floor((seconds%3600)/60)
+  let sec = (seconds%3600)%60
+  
+  
+  //With a little help for the format.
+  if (hours < 10){
+    hours = '0'+hours
+  }
+  if (minutes<10){
+    minutes = '0'+minutes
+  }
+   if (sec<10){
+    sec = '0'+sec
+  }
+  
+  return `${hours}:${minutes}:${sec}`;
+}
