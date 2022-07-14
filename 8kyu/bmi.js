be8b3c3eed75a782ced88b4bd66d3352f2a55be2
Matías Guarnera <76770019+matiasg11@users.noreply.github.com>
2022-07-14 12:23:55 -0300

    Create bmi.js

---
+++ 8kyu/bmi.js
@@ -0,0 +1,7 @@
+function bmi(weight, height) {
+  let index = weight / (height ** 2);
+  if (index <= 18.5) return "Underweight";
+  else if (index <= 25) return "Normal";
+  else if (index <= 30) return "Overweight";
+  else return "Obese";
+}
