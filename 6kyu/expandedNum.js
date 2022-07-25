0b712d2b3f6a823e4270b7b595d3f7f94a538ba5
Matías Guarnera <76770019+matiasg11@users.noreply.github.com>
2022-07-25 10:32:57 -0300

    Create expandedNum.js

---
+++ 6kyu/expandedNum.js
@@ -0,0 +1,16 @@
+function expandedForm(num) {
+  let number = String(num).split("")
+  let result = ""
+  
+  for (let i=0; i<number.length; i++){
+    if (number[i] != 0){
+      let partial = number[i] * (10**(number.length-1-i))
+      result += `${partial}`
+      if (i != (number.length-1)){
+        result += ` + `
+      }
+    }
+    
+  }
+  return  result.slice(-3) == " + " ? result.slice(0,-3) : result
+}
