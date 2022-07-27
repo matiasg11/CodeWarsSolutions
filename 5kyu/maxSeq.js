c6755254480688b37384d135c15581ea3282ee39
Matías Guarnera <76770019+matiasg11@users.noreply.github.com>
2022-07-27 12:32:37 -0300

    Create maxSeq.js

---
+++ 5kyu/maxSeq.js
@@ -0,0 +1,20 @@
+var maxSequence = function(arr){
+  if (arr == []){
+    return 0
+  }
+  
+  let partial = 0
+  let max = 0
+  
+  for(let i=0; i<arr.length; i++){
+    partial = 0
+    for (let j=i; j<arr.length; j++){
+      partial += arr[j]
+      if (partial>max){
+        max = partial
+    }
+  }
+  }
+  
+  return max
+}
