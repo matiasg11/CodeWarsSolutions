b0db77b0546f41ad6802f06662272e2fd739548e
Matías Guarnera <76770019+matiasg11@users.noreply.github.com>
2022-07-30 14:17:15 -0300

    Create alphabetPosition.js

---
+++ 6kyu/alphabetPosition.js
@@ -0,0 +1,9 @@
+function alphabetPosition(text) {
+
+  text = text.toLowerCase().split("")
+  text = text.filter((a)=> a!=" ")
+  
+  let result = text.map((a) => a.charCodeAt()-96)
+  result = result.filter((a)=> a>0 && a< 27)
+  return result.join(" ");
+}
