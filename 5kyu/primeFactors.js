f90ec048ac9eda76fd0794ffa06aa7a9a010719a
Matías Guarnera <76770019+matiasg11@users.noreply.github.com>
2022-07-30 14:06:09 -0300

    Create primeFactors.js

---
+++ 5kyu/primeFactors.js
@@ -0,0 +1,30 @@
+function primeFactors(n){
+  let arr = []
+   for (let i=2; i<=(n); i++){
+       if (n%i == 0 && isPrime(i) == true){
+         arr.push(i)
+         n = n/i
+         i=1
+       }
+     }
+   
+  let str = ""
+  for(let m=0; m<arr.length; m++){
+    let n = arr.lastIndexOf(arr[m]) - arr.indexOf(arr[m])
+    if (n > 0){
+      str = str + `(${arr[m]}**${n+1})`
+    }else{
+      str = str + `(${arr[m]})`
+    }
+    m = arr.lastIndexOf(arr[m])
+  }
+  return str
+}
+
+function isPrime(n) {
+  let rt = Math.sqrt(n);
+  for(let i = 2; i <= rt; i++) {
+    if(n % i === 0) return false; 
+  }
+  return n !== 1;
+}
