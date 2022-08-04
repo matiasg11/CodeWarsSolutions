3b367d54fbd40c6ef00cc1a4db94b46f36d1dc67
Matías Guarnera <76770019+matiasg11@users.noreply.github.com>
2022-08-04 11:48:14 -0300

    Create getAge.js

---
+++ 5kyu/getAge.js
@@ -0,0 +1,11 @@
+function getAge(birthDate, nowDate) {
+  if (birthDate == undefined || nowDate == undefined){
+    return 1
+  }
+  let age = nowDate.getFullYear() - birthDate.getFullYear()
+ if (nowDate.getMonth() < birthDate.getMonth() || (nowDate.getMonth() == birthDate.getMonth() && nowDate.getDate() < birthDate.getDate())) {
+  age--;
+}
+  
+  return age
+}
