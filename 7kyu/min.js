8464bdb62d128d8ff654958560f99e4437988ff2
Matías Guarnera <76770019+matiasg11@users.noreply.github.com>
2022-08-04 11:47:52 -0300

    Create min.js

---
+++ 7kyu/min.js
@@ -0,0 +1,16 @@
+function min(a, b){
+  if (a=== undefined || b===undefined){
+    return NaN
+  }
+  if(a===null){
+    a=0
+  }
+  if(b===null){
+    b=0
+  }
+  
+  if (isNaN(a) || isNaN(b)){
+    return NaN
+  }
+  return (a<b)?a:b;
+}
