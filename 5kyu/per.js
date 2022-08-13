ea5d48945a621efdd312e3fabf368439ed4e3893
Matías Guarnera <76770019+matiasg11@users.noreply.github.com>
2022-08-13 12:58:35 -0300

    Create per.js

---
+++ 5kyu/per.js
@@ -0,0 +1,8 @@
+function perimeter(n) {
+  let fibo = [1,1]
+  for(let i=2; i<=n; i++){
+    fibo.push(fibo[i-1]+fibo[i-2])
+  }
+  fibo = fibo.slice(0,n+1)
+    return fibo.reduce((a,b)=>a+b,0)*4 
+}
