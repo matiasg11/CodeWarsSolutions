c7577e4050a6a8694057279324697a724919bd56
Matias Guarnera <matiasguarnera@gmail.com>
2022-09-12 23:31:29 -0300

    intervals

--- 4kyu/intervals.js
+++ 4kyu/intervals.js
@@ -5,8 +5,10 @@ function sumIntervals(intervals) {
   let start = intervals[0][0]  //The lowest number of the intervals
   let result = 0
   for(let i=0; i<intervals.length; i++){ 
-    result += (Math.max(0, intervals[i][1]-Math.max(intervals[i][0], start)))  //We sum 0 if the max interval is negative or the interval between the right part and the highest number we've had in the intervals.
-    start = Math.max(intervals[i][1], start) //If the interval extends our current total interval, we put a new boundary. else, we continue as if nothing happened because the last interval fell within our big one.
+    result += (Math.max(0, intervals[i][1]-Math.max(intervals[i][0], start)))  
+    //We sum 0 if the max interval is negative or the interval between the right part and the highest number we've had in the intervals.
+    start = Math.max(intervals[i][1], start) 
+    //If the interval extends our current total interval, we put a new boundary. else, we continue as if nothing happened because the last interval fell within our big one.
   }
   return result
   
