7a526aa6423962366fa7c45e79234f644f2f69ed
Matías Guarnera <76770019+matiasg11@users.noreply.github.com>
2022-09-21 14:23:19 -0300

    Create codeVowelsNumbers.js

---
+++ 6kyu/codeVowelsNumbers.js
@@ -0,0 +1,9 @@
+// turn vowels into numbers
+function encode(string){
+  return string.replace(/[aeiou]/g, function (x) { return '_aeiou'.indexOf(x) });
+}
+
+//turn numbers back into vowels
+function decode(string){
+  return string.replace(/[1-5]/g, function (x) { return '_aeiou'.charAt(x) });
+}
