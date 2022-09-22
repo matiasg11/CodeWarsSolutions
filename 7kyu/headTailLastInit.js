cd86ce4e5509239c2188dbf27bb340b1ec63d61d
Matías Guarnera <76770019+matiasg11@users.noreply.github.com>
2022-09-22 19:56:41 -0300

    Create headTailLastInit.js

---
+++ 7kyu/headTailLastInit.js
@@ -0,0 +1,12 @@
+function head(arr) {
+  return arr[0];
+}
+function tail(arr) {
+  return arr.slice(1);
+}
+function last(arr) {
+  return arr[arr.length - 1];
+}
+function init(arr) {
+  return arr.slice(0, arr.length - 1);
+}
