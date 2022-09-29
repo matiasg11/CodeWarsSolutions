adb1f2b4c979e412e6fc25d58ff4aea707532566
Matias Guarnera <matiasguarnera@gmail.com>
2022-09-28 23:35:14 -0300

    Finished 6kyu exercise

--- 6kyu/codeVowelsNumbers.js
+++ 6kyu/codeVowelsNumbers.js
@@ -3,6 +3,7 @@ function encode(string){
   return string.replace(/[aeiou]/g, function (x) { return '_aeiou'.indexOf(x) });
 }
 
+
 //turn numbers back into vowels
 function decode(string){
   return string.replace(/[1-5]/g, function (x) { return '_aeiou'.charAt(x) });
