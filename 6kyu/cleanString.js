6ffb3bdfd0769f350f63c9c6380eedf3c1ab9db4
Matías Guarnera <76770019+matiasg11@users.noreply.github.com>
2022-09-30 10:56:09 -0300

    Create cleanString.js

---
+++ 6kyu/cleanString.js
@@ -0,0 +1,15 @@
+function cleanString(s) {
+  let str = s.split("")
+  for (let i=0; i<str.length; i++){
+    if (i != 0 && str[i] == "#"){
+      str.splice(i-1,2)
+      i -= 2
+    }
+    if (str[i] == "#"){
+      str.splice(i,1)
+    }
+    
+  }
+  return str.join("").replace(/#/gi,"")
+
+}
