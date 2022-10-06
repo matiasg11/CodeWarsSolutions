2a21977292bfc025ef5c398973457161f6ed9cfd
Matías Guarnera <76770019+matiasg11@users.noreply.github.com>
2022-10-06 20:50:57 -0300

    Create noSeRecienCaigo.js

---
+++ 8kyu/noSeRecienCaigo.js
@@ -0,0 +1,12 @@
+function sc(floor){
+
+  if (floor > 6) {
+    return "Aa~ ".repeat(floor - 1) + "Pa!";
+  } else if (floor > 1) {
+    return "Aa~ ".repeat(floor - 1) + "Pa! Aa!";
+  } else {
+    return "";
+  }
+  
+}
+6 similar c
