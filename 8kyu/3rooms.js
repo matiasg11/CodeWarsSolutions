984eaa742beab41343485f87a5aed75925afe22f
Matías Guarnera <76770019+matiasg11@users.noreply.github.com>
2022-10-06 20:47:41 -0300

    Create 3rooms.js

---
+++ 8kyu/3rooms.js
@@ -0,0 +1,17 @@
+const rooms = {
+  bedroom: {
+    name: 'Bedroom',
+    description: 'For... sleeping',
+    completed: true,
+  },
+  bathroom: {
+    name: 'Bathroom',
+    description: 'This will get you clean',
+    completed: false,
+  },
+  kitchen: {
+    name: 'Kitchen',
+    description: 'Hungry? Why wait?',
+    completed: true,
+  },
+}
