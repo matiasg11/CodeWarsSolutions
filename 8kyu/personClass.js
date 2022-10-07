a5944dd0536d44d14faeb5d90bc42bcb3a33aae7
Matías Guarnera <76770019+matiasg11@users.noreply.github.com>
2022-10-07 18:27:34 -0300

    Create personClass.js

---
+++ 8kyu/personClass.js
@@ -0,0 +1,16 @@
+class Person {
+  constructor(firstName, lastName, age, gender){
+    this.firstName = firstName || "John"
+    this.lastName = lastName || "Doe"
+    this.age = age || 0
+    this.gender = gender || "Male"
+  }
+  sayFullName(){
+    return `${this.firstName} ${this.lastName}`
+  }
+
+  
+}  
+Person.greetExtraTerrestrials = function(raceName){
+    return `Welcome to Planet Earth ${raceName}`
+  }
