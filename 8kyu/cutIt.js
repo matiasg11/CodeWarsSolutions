3b984666109b357ae3a39b48f8dcfeba55f06c86
Matías Guarnera <76770019+matiasg11@users.noreply.github.com>
2022-10-10 10:41:52 -0300

    Create cutIt.js

---
+++ 8kyu/cutIt.js
@@ -0,0 +1,19 @@
+function cutIt(arr){
+  var newArray = [];
+  var shortest = "";
+  
+  for (var i = 0; i < arr.length ; i++) {
+    var currentStr = arr[i];
+    
+    if (currentStr.length < shortest.length || shortest.length === 0) {
+      shortest = currentStr;
+    }
+  }
+  
+  for (var j = 0; j < arr.length; j++) {
+    var currentStr2 = arr[j];
+    newArray.push(currentStr2.slice(0, shortest.length));
+  }
+  
+  return newArray;
+}
