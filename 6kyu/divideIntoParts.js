e989bf5c7bb6fb17e470019f7932e5028a6acbb6
Matias Guarnera <matiasguarnera@gmail.com>
2022-10-19 23:42:24 -0300

    Finished 6kyu exercise

--- 6kyu/divideIntoParts.js
+++ 6kyu/divideIntoParts.js
@@ -3,6 +3,7 @@
 Example code:
 
 splitInteger(20, 6)  // returns [3, 3, 3, 3, 4, 4]
+
 Complete the function so that it returns an array of integer representing the parts. Ignoring the order of the parts, there is only one valid solution for each input to your function!
 
 (Also, there is no reason to test for edge cases: the input to your function will always be valid for this kata.)*/
