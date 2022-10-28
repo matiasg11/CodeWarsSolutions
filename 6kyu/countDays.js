93a393e67f45b2462868c23e44b3de0a40b1241f
Matias Guarnera <matiasguarnera@gmail.com>
2022-10-27 23:46:26 -0300

    Finished 6kyu exercise

--- 6kyu/countDays.js
+++ 6kyu/countDays.js
@@ -6,6 +6,7 @@ function countDays(d){
   const diffTime = Math.abs(d - date);
   const diffDays = Math.round(diffTime / (1000 * 60 * 60 * 24)); 
   console.log(diffDays, diffTime)
+  
   if (dday[2]<day[2] || dday[2]==day[2] && dday[1]<day[1] || dday[2]==day[2] && dday[1] == day[1] && dday[0]<day[0]){
     return "The day is in the past!"
   } else if (dday[2]==day[2] && dday[1]==day[1] && dday[0]==day[0]){
